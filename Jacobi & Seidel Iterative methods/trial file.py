5cf03f818515762482d4e5029ddfa1f9910a0e44
mohamedezzat2000z <mohamedezzat2000z@gmail.com>
2021-12-18 19:45:19 +0200

    trial file

---
+++ Jacobi & Seidel Iterative methods/trial file.py
@@ -0,0 +1,19 @@
+import Jacobi_method
+import Seidel_method
+coeffarray = [
+[12, 3, 5,3],
+[1, 5, 3,9],
+[3, 7, 13,9]] 
+numofvar = 3
+iterMax = 100
+initalguess = [1,1,1]
+errorStop = 10**(-1)
+jsolver = Jacobi_method.jacobiSolver(coeffarray,iterMax,initalguess,errorStop,5)
+print('Jacobi answer\n')
+jsolver.Solve()
+print("=========================================\n")
+
+seidSolver = Seidel_method.SeidelSolver(coeffarray,iterMax,initalguess,errorStop,5)
+print('Seidel answer\n')
+
+seidSolver.Solve()
