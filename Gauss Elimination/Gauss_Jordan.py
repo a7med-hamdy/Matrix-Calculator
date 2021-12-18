c65940f30809014b16e87c7e7b31397c0dfcda49
Ahmed-Ashour-2024 <mody.ashour.726@gmail.com>
2021-12-18 23:09:24 +0200

    handled special cases + added execution time & roundin for Gausses

--- Gauss Elimination/Gauss_Jordan.py
+++ Gauss Elimination/Gauss_Jordan.py
@@ -1,19 +1,14 @@
 import Pivoting
-import time as t
+from sigfig import round
+from timeit import default_timer as timer
 #Gauss-Jordan:
 #   Forward Elimination
 #   Backward Elimination
 
 class Gauss_Jordan():
-    #constructor
-    def init(self, n, A, B):
-        self.n = n
-        self.A = A
-        self.B = B
 
-    #solving function
-    def solve(n, A, B):
-        begin_time = t.time() #measure the execution time
+    def solve(n, A, B, precision = 5):
+        begin_time = timer() #measure the execution time
         iterations = 0 #number of iterations counter
         #Forward Elimination
         factor = 0
@@ -22,34 +17,49 @@ class Gauss_Jordan():
             A, B = Pivoting.pivoting(n, i, A, B) #search for pivoting in each iteration
             for j in range(i+1, n):
                 iterations += 1
-                factor = A[j][i] / A[i][i]
+                #if dividing by zero occurs that means -> infinite solutions (variable eliminated)
+                try:
+                    factor = round(A[j][i] / A[i][i], sigfigs = precision)
+                except:
+                    return "The system has infinite number of solutions"
                 for k in range(i, n):
                     iterations += 1
-                    A[j][k] = A[j][k] - factor * A[i][k]
-                B[j] = B[j] - factor * B[i]
-
+                    A[j][k] = round(A[j][k] - factor * A[i][k], sigfigs = precision)
+                B[j] = round(B[j] - factor * B[i], sigfigs = precision)
+        #Check if the system has no unique solution
+        if(A[-1][-1] == 0):    
+            if( B[-1] == 0 ):
+                return "The system has infinite number of solutions"
+            else:
+                return "The system has no solution"
         #Backward Elimination
         for i in range(n-1, 0, -1):
             iterations += 1
             for j in range(i-1, -1, -1):
                 iterations += 1
-                factor = A[j][i] / A[i][i]
+                factor = round(A[j][i] / A[i][i], sigfigs = precision)
                 for k in range(i, -1, -1):
                     iterations += 1
-                    A[j][k] = A[j][k] - factor * A[i][k]
-                B[j] = B[j] - factor * B[i]
+                    A[j][k] = round(A[j][k] - factor * A[i][k], sigfigs = precision)
+                B[j] = round(B[j] - factor * B[i], sigfigs = precision)
         print(A, B)
         #substitution
         X = [0] * n
         for i in range(n):
             iterations += 1
-            X[i] = B[i] / A[i][i]
-        print("---> %s seconds <---" % (t.time() - begin_time))
+            X[i] = round(B[i] / A[i][i], sigfigs = precision)
+        time = round(timer() - begin_time, sigfigs = precision)
         print('X= ', X)
+        print("-> %s seconds <-" % time)
         print('number of iterations = ', iterations)
-        return X
+        return X, iterations, time
+    #End solve
 
 #debugging
-Gauss_Jordan.solve(3, [[2,1,4],
-                       [1,2,3],
-                       [4,-1,2]], [1,1.5,2])
+print(Gauss_Jordan.solve(3, [[2,1,4],
+                             [1,2,3],
+                             [4,-1,2]], [1,1.5,2], 5)) #unique solution
+print('-----------------------------------------------------------')
+print(Gauss_Jordan.solve(3, [[1,1,1],
+                             [0,1,-3],
+                             [2,1,5]], [2,1,0], 3)) #no solution
