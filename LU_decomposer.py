ddec9fef271234fa4967fe56ed92e84b71ffcaee
a7med-hamdy <hamdiahmed906@gmail.com>
2021-12-20 09:56:55 +0200

    lu done

--- LU_decomposer.py
+++ LU_decomposer.py
@@ -7,7 +7,6 @@ class LU_decomposer:
     n = 0 #length of the matrix
     precision = 0 # percision number of significant figures
     vectorB = [] # the b vector 
-    iteration_counter = 0
     def __init__(self,x,tol,b):
         self.matrix = x
         self.n = x.shape[0]
@@ -15,21 +14,31 @@ class LU_decomposer:
         self.vectorB = b
 
     def decompose(self, type):
+        iterations = 0
         start = time.perf_counter()
-        self.iteration_counter = 0
         if(type == "dolittle"):
             lower,upper,identity = self.DollitleDecompose()
             self.vectorB = np.matmul(identity,self.vectorB)
+            iterations += self.n**3/3
         elif(type == "cholesky"):
-            self.CholeskyDecompose()
+            a=np.array(self.matrix,float)
+            if(not self.check_positive(a) or not self.check_symmetric(a)):
+                return "not valid cholesky"
+            else:
+                lower, upper = self.CholeskyDecompose()
+                iterations += self.n**3/6
         elif(type == "crout"):
             lower,upper = self.CroutDecompose()
+            iterations += self.n**3/3
         else:
-            lower, upper = print("invalid decomposition type")
+            return "invalid decomposition type"
+        x = self.subistitute(upper,lower)
+        iterations += self.n*(self.n-1)/2+self.n-1+self.n**3/3
         end = time.perf_counter()
-        print(np.matmul(lower,upper))
-        print(f'execution time for decomposition : {end-start}')
-        print(self.subistitute(upper,lower))
+        if(isinstance(x, str)):
+            return x
+        else :
+            return [x, end-start, iterations]
 
     ## functiont to check that the array is Symitric
     def check_symmetric(self,a, rtol=1e-05, atol=1e-08):
@@ -39,65 +48,58 @@ class LU_decomposer:
     ## functiont to check that the array is postive definte
     def check_positive(self,x):
         for i in np.linalg.eigvals(x):
-            if i>0:
+            if i<0:
                 return False
-            return True
+        return True
 
     ## cholesky decompostoin function
     def CholeskyDecompose(self):
         a=np.array(self.matrix,float)
-        if(not self.check_positive(a) or not self.check_symmetric(a)):
-            print("not valid cholesky")
-            return 
-        L=np.zeros_like(a)
-        z=0
+        L=np.zeros_like(a, float)
         for j in range(self.n):
-                
             for i in range(j,self.n):
-                if i==j:
+                if(i==j):
                     summ=0
                     for l in range(j):
                         summ = round(summ+ L[i,l]**2 , self.precision)
-                        z+=1
-                            
-                        L[i,j] = round(np.sqrt(a[i,j]-summ), self.precision)
-                        
+                    L[i,j] = round(np.sqrt(a[i,j]-summ), self.precision)
                 else:
                     summ=0
                     for l in range(j):
-                        summ=round(summ+L[i,l]*L[j,l], self.precision)
-                        z+=1
-                            
-                    L[i,j]=round(round(a[i,j]-summ, self.precision)/L[j,j], self.precision)
-                        
-        
+                        summ=round(summ+L[i,l]*L[j,l], self.precision) 
+                    L[i,j]=round(round(a[i,j]-summ, self.precision)/L[j,j], self.precision)            
+        return (L,L.T)        
 
     def CroutDecompose(self):
-        lower = []
-        upper = []
+        Lower = []
+        Upper = []
         sum = 0
         #do not decompose if matrix is not square
-        the_list = self.matrix.tolist()
-        for i in range(len(the_list)):
-            lower.append([0 for i in range(len(the_list))])
-            upper.append([0 for i in range(len(the_list))])
-            if len(the_list[i]) != len(the_list):
+        coeffArray = self.matrix.tolist()
+        for i in range(len(coeffArray)):
+            Lower.append([0 for i in range(len(coeffArray))])
+            Upper.append([0 for i in range(len(coeffArray))])
+            if len(coeffArray[i]) != len(coeffArray):
                 return "Error, not a square matrix"
+        
+        n = len(coeffArray)
 
-        for i in range(0, len(the_list)):
-            upper[i][i]  = 1
-            sum = 0
-            for j in range(i, len(the_list)):
-                for k in range(0, i):
-                    sum += round(lower[j][k] * upper[k][i], self.precision)
-                lower[j][i] = round(the_list[j][i] - sum, self.precision) 
-            sum = 0
-            for j in range(i+1, len(the_list)):
-                for k in range(0, i):
-                    sum += round(lower[i][k] * upper[k][j], self.precision) 
-                upper[i][j] = round((the_list[i][j] - sum) / lower[i][i], self.precision)
+        for j in range(len(coeffArray)):
+            Upper[j][j] = 1             
+            for i in range(j, len(coeffArray)):  
+                sum = coeffArray[i][j]
+                for k in range(j):
+                    sum -= Lower[i][k]*Upper[k][j]
+                Lower[i][j] = sum
+            for i in range(j+1, len(coeffArray)):
+                sumU = float(coeffArray[j][i])
+                for k in range(j):
+                    sumU -= Lower[j][k]*Upper[k][i]
 
-        return np.array(upper,np.float64),np.array(lower,np.float64)
+                Upper[j][i] = sumU/Lower[j][j]
+
+        array = np.dot(Lower,Upper)
+        return np.array(Lower,np.float64),np.array(Upper,np.float64)
 
     
     def DollitleDecompose(self):
@@ -106,7 +108,6 @@ class LU_decomposer:
         for i in range(self.n):
             scaling.append(abs(self.matrix[i,0]))
             for j in range(1,self.n):
-                self.iteration_counter +=1
                 if(abs(self.matrix[i,j]) > scaling[i]):
                     scaling[i] = abs(self.matrix[i,j])
         lower = np.zeros((self.n,self.n), np.float64)
@@ -116,7 +117,6 @@ class LU_decomposer:
                 factor = round(self.matrix[i,k]/self.matrix[k,k], self.precision)
                 lower[i,k] = factor
                 for j in range(k,self.n):
-                    self.iteration_counter +=1
                     self.matrix[i,j] = round(self.matrix[i,j]-round(factor*self.matrix[k,j], self.precision), self.precision)
         np.fill_diagonal(lower,1)
         return lower,self.matrix,identity
@@ -135,16 +135,37 @@ class LU_decomposer:
             lower[[pos,k]] = lower[[k,pos]]
         return lower,upper,identity
 
+    def solutionsChecker(self, matrix, vector):
+        z = np.all(matrix == 0, axis = 1)
+        z = z.tolist()
+        for i in z:
+            if(z[i] == True):
+                if(vector[i] == 0):
+                    return 0
+                else:
+                    return 1
+        return 2
     def subistitute(self,upper,lower):
         #forward subistitution
+        value = self.solutionsChecker(lower, self.vectorB)
+        if(value == 0):
+            return "infinite solutions"
+        elif(value == 1):
+            return "no solution"
         y = [0]*self.n
-        y[0] = round(self.vectorB[0], self.precision)
+        y[0] = round(self.vectorB[0]/lower[0,0], self.precision)
         for i in range(1,self.n):
             sum = self.vectorB[i]
-            for j in range(0,i):
+            j = 0
+            for j in range(i):
                 sum = round(sum - round(lower[i,j]*y[j], self.precision), self.precision)
-            y[i] = sum
+            y[i] = sum / lower[i,j+1]
         #backward subistitution
+        value = self.solutionsChecker(upper, y)
+        if(value == 0):
+            return "infinite solutions"
+        elif(value == 1):
+            return "no solution"
         x = [0]*self.n
         x[self.n-1] = round(y[self.n-1] / upper[self.n-1,self.n-1], self.precision)
         i = self.n-1
@@ -159,7 +180,9 @@ class LU_decomposer:
 
 
 def main():
-    x = LU_decomposer(np.array([[25,5,1],[64,8,1],[144,12,1]], np.float64),20,[106.8,177.2,279.2])
-    x.decompose("cholesky")
+    # x = LU_decomposer(np.array([[25,5,1],[64,8,1],[144,12,1]], np.float64),5,[106.8,177.2,279.2])
+    # x = LU_decomposer(np.array([[6,15,55],[15,55,225],[55,225,979]], np.float64),20,[76,295,1259])
+    x = LU_decomposer(np.array([[1,2],[2,4]], np.float64),20,[4,8])
+    x.decompose("dolittle")
 if __name__ == "__main__":
     main()
