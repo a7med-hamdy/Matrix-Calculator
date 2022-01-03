388b4c6ec3bb4e5f5857d57d30ce24cfa5d37b24
a7med-hamdy <hamdiahmed906@gmail.com>
2022-01-03 14:19:37 +0200

    added iterations

--- bracketingMethodSolver.py
+++ bracketingMethodSolver.py
@@ -4,45 +4,51 @@ import time
 
 class bracketingMethodSolver:
     '''
-    params: lower bound / upper bound / tolerance(Es) / function / number of significant figures
+    params: lower bound / upper bound / tolerance(Es) / function / number of significant figures / no. of iterations
     returns: lower bound list / upper bound list / number of iterations / root(xr) / function used / time
     '''
-    def bisect(self,a,b,tol,functionString,n):
+    def bisect(self,a,b,tol,functionString,n, max_iterations):
         start = time.perf_counter()
         f = sympify(functionString)
         if f(a)*f(b) >= 0:
             return "the two roots are either positive or negative"
+        if f(a)>0:
+            upper=a
+            lower=b
+        else:
+            upper=b
+            lower=a
         xr = 0
         i = 0
-        a_list = []
-        b_list = []
-        a_list.append(a)
-        b_list.append(b)
-        while(f(xr) != 0):
+        upper_List = []
+        lower_List = []
+        upper_List.append(upper)
+        lower_List.append(lower)
+        while(f(xr) != 0 and i < max_iterations):
             xrnew = round(round((a+b),n)/2.0,n)
             i += 1
-            print(f'lower limit : {a} | upper limit : {b} | root: {xrnew} | f(a) = {f(a)} | f(b) = {f(b)} | f(xr) = {f(xrnew)} | error = {abs(xrnew-xr)}')
+            print(f'lower limit : {lower} | upper limit : {upper} | root: {xrnew} | f(lower) = {f(lower)} | f(upper) = {f(upper)} | f(xr) = {f(xrnew)} | error = {abs(xrnew-xr)}')
             if(abs(xr-xrnew) < tol):
                 xr = xrnew
                 break
             value = f(a)*f(xrnew)
             xr = xrnew
             if(value < 0):
-                b = xrnew
+                upper = xrnew
             elif(value > 0):
-                a=xrnew
+                lower =xrnew
             else:
                 break
-            a_list.append(a)
-            b_list.append(b)
+            upper_List.append(upper)
+            lower_List.append(lower)
         print(f'xr = {xr} and no. of iterations = {i}')
         end = time.perf_counter()
-        return list(a_list,b_list,i,xr,f,end - start)
+        return list(lower_List,upper_List,i,xr,f,end - start)
     '''
-    params: lower bound / upper bound / tolerance(Es) / function / number of significant figures
+    params: lower bound / upper bound / tolerance(Es) / function / number of significant figures / no. of iterations
     returns: lower bound list / upper bound list / number of iterations / root(xr) / function used / time
     '''
-    def regula(self,a,b,tol,functionString,n):
+    def regula(self,a,b,tol,functionString,n, max_iterations):
         start = time.perf_counter()
         f = sympify(functionString)
         upper=0.0
@@ -83,7 +89,7 @@ class bracketingMethodSolver:
                 lower=MR2
             upper_List.append(upper)
             lower_List.append(lower)
-            if(abs(MR2-MR1)<tol):
+            if(abs(MR2-MR1)<tol or itr > max_iterations):
                 end = time.perf_counter()
                 return list(lower_List,upper_List,itr,MR2,f, end - start)
             else:
