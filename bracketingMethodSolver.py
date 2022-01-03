bb5376d1f85bb04b0d7e5367b7212d4ae0c58164
a7med-hamdy <hamdiahmed906@gmail.com>
2022-01-03 13:52:04 +0200

    fixes

--- bracketingMethodSolver.py
+++ bracketingMethodSolver.py
@@ -1,16 +1,15 @@
 from sigfig import round
-import numpy as np
 from sympy import *
 
 class bracketingMethodSolver:
     '''
-    params: lower bound / upper bound / tolerance(Es) / function
-    returns: lower bound list / upper bound list / number of iterations / root(xr)
+    params: lower bound / upper bound / tolerance(Es) / function / number of significant figures
+    returns: lower bound list / upper bound list / number of iterations / root(xr) / function used
     '''
-    def bisect(self,a,b,tol,f):
+    def bisect(self,a,b,tol,functionString,n):
+        f = sympify(functionString)
         if f(a)*f(b) >= 0:
-            print("no roots here")
-            return None
+            return "the two roots are either positive or negative"
         xr = 0
         i = 0
         a_list = []
@@ -18,7 +17,7 @@ class bracketingMethodSolver:
         a_list.append(a)
         b_list.append(b)
         while(f(xr) != 0):
-            xrnew = (a+b)/2.0
+            xrnew = round(round((a+b),n)/2.0,n)
             i += 1
             print(f'lower limit : {a} | upper limit : {b} | root: {xrnew} | f(a) = {f(a)} | f(b) = {f(b)} | f(xr) = {f(xrnew)} | error = {abs(xrnew-xr)}')
             if(abs(xr-xrnew) < tol):
@@ -35,22 +34,22 @@ class bracketingMethodSolver:
             a_list.append(a)
             b_list.append(b)
         print(f'xr = {xr} and no. of iterations = {i}')
-        return a_list,b_list,i,xr
+        return list(a_list,b_list,i,xr,f)
     '''
-    params: lower bound / upper bound / tolerance(Es) / function
-    returns: lower bound list / upper bound list / number of iterations / root(xr)
+    params: lower bound / upper bound / tolerance(Es) / function / number of significant figures
+    returns: lower bound list / upper bound list / number of iterations / root(xr) / function used
     '''
-    def regula(self,a,b,Es,f):
+    def regula(self,a,b,tol,functionString,n):
+            f = sympify(functionString)
             upper=0.0
             lower=0.0
             MR1=0.0
             MR2=0.0
             upper_List = []
             lower_List = []
-            itr =1
+            itr = 1
             if f(a)*f(b) >= 0:
-                print("roots problem")
-                return None
+                return "the two roots are either positive or negative"
             if f(a)>0:
                 upper=a
                 lower=b
@@ -60,9 +59,9 @@ class bracketingMethodSolver:
             upper_List.append(upper)
             lower_List.append(lower)
             MR1=((lower*f(upper))-(f(lower)*upper))/(f(upper)-f(lower))
-            MR1 = MR1
+            MR1 = round(MR1, n)
             FMR1 = f(MR1)
-            FMR1 = FMR1
+            FMR1 = round(FMR1,n)
             if f(lower)*FMR1 < 0:
                 upper=MR1
             elif f(upper)*FMR1 < 0:
@@ -71,17 +70,17 @@ class bracketingMethodSolver:
             while(True):
                 itr=itr+1
                 MR2=((lower*f(upper))-(f(lower)*upper))/(f(upper)-f(lower))
-                MR2 = MR2
+                MR2 = round(MR2, n)
                 FMR2 = f(MR2)
-                FMR2 = FMR2
+                FMR2 = round(FMR2, n)
                 if f(lower)*FMR2 < 0:
                     upper=MR2
                 elif f(upper)*FMR2 < 0:
                     lower=MR2
                 upper_List.append(upper)
                 lower_List.append(lower)
-                if(abs(MR2-MR1)<Es):
-                    return lower_List,upper_List,itr,MR2
+                if(abs(MR2-MR1)<tol):
+                    return list(lower_List,upper_List,itr,MR2,f)
                 else:
                     MR1=MR2
 
