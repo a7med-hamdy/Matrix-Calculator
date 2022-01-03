74c9c25433bf0a195963d0a9510a8b78e7a7c5d2
a7med-hamdy <hamdiahmed906@gmail.com>
2022-01-03 14:05:14 +0200

    time added

--- bracketingMethodSolver.py
+++ bracketingMethodSolver.py
@@ -1,12 +1,14 @@
 from sigfig import round
 from sympy import *
+import time
 
 class bracketingMethodSolver:
     '''
     params: lower bound / upper bound / tolerance(Es) / function / number of significant figures
-    returns: lower bound list / upper bound list / number of iterations / root(xr) / function used
+    returns: lower bound list / upper bound list / number of iterations / root(xr) / function used / time
     '''
     def bisect(self,a,b,tol,functionString,n):
+        start = time.perf_counter()
         f = sympify(functionString)
         if f(a)*f(b) >= 0:
             return "the two roots are either positive or negative"
@@ -34,53 +36,56 @@ class bracketingMethodSolver:
             a_list.append(a)
             b_list.append(b)
         print(f'xr = {xr} and no. of iterations = {i}')
-        return list(a_list,b_list,i,xr,f)
+        end = time.perf_counter()
+        return list(a_list,b_list,i,xr,f,end - start)
     '''
     params: lower bound / upper bound / tolerance(Es) / function / number of significant figures
-    returns: lower bound list / upper bound list / number of iterations / root(xr) / function used
+    returns: lower bound list / upper bound list / number of iterations / root(xr) / function used / time
     '''
     def regula(self,a,b,tol,functionString,n):
-            f = sympify(functionString)
-            upper=0.0
-            lower=0.0
-            MR1=0.0
-            MR2=0.0
-            upper_List = []
-            lower_List = []
-            itr = 1
-            if f(a)*f(b) >= 0:
-                return "the two roots are either positive or negative"
-            if f(a)>0:
-                upper=a
-                lower=b
-            else:
-                upper=b
-                lower=a
+        start = time.perf_counter()
+        f = sympify(functionString)
+        upper=0.0
+        lower=0.0
+        MR1=0.0
+        MR2=0.0
+        upper_List = []
+        lower_List = []
+        itr = 1
+        if f(a)*f(b) >= 0:
+            return "the two roots are either positive or negative"
+        if f(a)>0:
+            upper=a
+            lower=b
+        else:
+            upper=b
+            lower=a
+        upper_List.append(upper)
+        lower_List.append(lower)
+        MR1=((lower*f(upper))-(f(lower)*upper))/(f(upper)-f(lower))
+        MR1 = round(MR1, n)
+        FMR1 = f(MR1)
+        FMR1 = round(FMR1,n)
+        if f(lower)*FMR1 < 0:
+            upper=MR1
+        elif f(upper)*FMR1 < 0:
+            lower=MR1
+        
+        while(True):
+            itr=itr+1
+            MR2=((lower*f(upper))-(f(lower)*upper))/(f(upper)-f(lower))
+            MR2 = round(MR2, n)
+            FMR2 = f(MR2)
+            FMR2 = round(FMR2, n)
+            if f(lower)*FMR2 < 0:
+                upper=MR2
+            elif f(upper)*FMR2 < 0:
+                lower=MR2
             upper_List.append(upper)
             lower_List.append(lower)
-            MR1=((lower*f(upper))-(f(lower)*upper))/(f(upper)-f(lower))
-            MR1 = round(MR1, n)
-            FMR1 = f(MR1)
-            FMR1 = round(FMR1,n)
-            if f(lower)*FMR1 < 0:
-                upper=MR1
-            elif f(upper)*FMR1 < 0:
-                lower=MR1
-            
-            while(True):
-                itr=itr+1
-                MR2=((lower*f(upper))-(f(lower)*upper))/(f(upper)-f(lower))
-                MR2 = round(MR2, n)
-                FMR2 = f(MR2)
-                FMR2 = round(FMR2, n)
-                if f(lower)*FMR2 < 0:
-                    upper=MR2
-                elif f(upper)*FMR2 < 0:
-                    lower=MR2
-                upper_List.append(upper)
-                lower_List.append(lower)
-                if(abs(MR2-MR1)<tol):
-                    return list(lower_List,upper_List,itr,MR2,f)
-                else:
-                    MR1=MR2
+            if(abs(MR2-MR1)<tol):
+                end = time.perf_counter()
+                return list(lower_List,upper_List,itr,MR2,f, end - start)
+            else:
+                MR1=MR2
 
