6e36a0a881981d415ecbd1366ba9a0fc42b23286
mohamedezzat2000z <mohamedezzat2000z@gmail.com>
2022-01-03 19:16:08 +0200

    fixed point

--- project.py
+++ project.py
@@ -232,7 +232,7 @@ def plotnormal(intial,driv):
     widget.destroy()
    x = Symbol('x')
    y = Symbol('y',real = True,positive = True)
-   function2 = lambdify(x,driv)
+   function2 = lambdify(y,driv)
 
    Xaxis = np.linspace(intial-7,intial+7,10*10)
    ylist=[]
